59f03676491a76e69127dd1ecaa766eeae8b90df
eaglesakura <eagle.sakura@gmail.com>
2018-03-25 20:35:26 +0900

    mod: Use godotenv library at env.list file parse.

--- env_list.go
+++ env_list.go
@@ -1,9 +1,8 @@
 package gaeenv
 
 import (
-	"bufio"
 	"bytes"
-	"io"
+	"github.com/joho/godotenv"
 	"io/ioutil"
 	"strings"
 )
@@ -34,30 +33,17 @@ func parseEnvLine(line string) (key, value string) {
 この関数はパースのみを行い、適用方法は呼び出しに任せる.
 */
 func parseEnvList(required bool, envFileName string) (map[string]string, error) {
-	buf, err := ioutil.ReadFile(envFileName)
+	result := map[string]string{}
+	raw, err := ioutil.ReadFile(envFileName)
+	if err == nil {
+		result, err = godotenv.Parse(bytes.NewReader(raw))
+	}
 	if err != nil {
 		if required {
-			// 読み込みに失敗した場合は必須チェック
 			return nil, err
-		}
-		return map[string]string{}, nil
-	}
-
-	result := map[string]string{}
-
-	reader := bufio.NewReader(bytes.NewReader(buf))
-	for {
-		line, _, err := reader.ReadLine()
-		if err != nil {
-			if err == io.EOF {
-				return result, nil
-			} else {
-				return nil, err
-			}
-		}
-		key, value := parseEnvLine(string(line))
-		if key != "" {
-			result[key] = value
+		} else {
+			return result, nil
 		}
 	}
+	return result, nil
 }
